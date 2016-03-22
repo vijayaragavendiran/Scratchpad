75b75522272d9910c9a856874f94fac20b2e2584
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-21 23:43:42 -0700

    adding notes to scratch pad

--- app.js
+++ app.js
@@ -58,12 +58,14 @@ var Note = React.createClass({
       getInitialState:function(){
         return{
           notes:[
-            'Call Bill',
-            'Email Lisa',
-            'Make appointment',
-            'Send Proposal'
+
           ]
         }
+      },
+      add:function(text){
+        var arr = this.state.notes;
+        arr.push(text);
+        this.setState({notes:arr});
       },
       update:function(newText,i){
         var arr = this.state.notes;
@@ -87,6 +89,9 @@ var Note = React.createClass({
       render:function(){
         return (<div className="board">
         {this.state.notes.map(this.eachNote)}
+        <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
+        onClick={this.add.bind(null,'New Note')}
+        />
         </div>
       );
       }
